9b0e3019b2b07bc6ae08dc4a20a052d5eed3791b
Henrique Moreira <henriquemoreiraa@gmail.com>
2020-05-22 21:27:43 -0300

    Leitura do financiamento

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -10,7 +10,8 @@ public class programa {
 		Scanner sc = new Scanner(System.in);
 		
 		String nome;
-		double salario;
+		double salario, porcentagemEntrada, ps, valorTotal, porcentagemValorTotal, valorMensal, valorFinalpago;
+		int prestacoes, opcao;
 		
 		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
 		System.out.print("Nome do cliente: ");
@@ -22,6 +23,27 @@ public class programa {
 			System.out.print("O salário não pode ser negativo, digite novamente: ");
 			salario = sc.nextDouble();
 		}
+		
+		System.out.print("Numero de prestacoes: ");
+		prestacoes = sc.nextInt();
+		
+		System.out.print("Porcentagem de entrada: ");
+		porcentagemEntrada = sc.nextDouble();
+		
+		System.out.print("Valor total financiado: ");
+		valorTotal = sc.nextDouble();
+		
+		do {
+		valorFinalpago = valorTotal - ((valorTotal * porcentagemEntrada)/100);
+		System.out.println("Valor apos a porcentagem de entrada :" +valorFinalpago);
+		valorMensal = valorFinalpago / prestacoes;
+		System.out.println("valor de casa prestacao : " + valorMensal);
+		porcentagemValorTotal = (valorMensal * 30)/ 100;
+		System.out.println("trinta por cento do valor pago mensalmente " + porcentagemValorTotal);
+		ps = (salario * 30)/100;
+		System.out.println("trinta por cento do salario mensal : " +  ps);
+		} while (porcentagemValorTotal > ps);
+		
 		
 		sc.close();
 	}
